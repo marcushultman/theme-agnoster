6cfcf8aeaa4ef3a4e92548df17f50bd11246891d
Marcus Hultman <marcus.hultman1@gmail.com>
2020-08-10 16:45:30 +0200

    differentiate between staged and unstaged dirty git states

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -70,19 +70,18 @@ set -q theme_svn_prompt_enabled; or set theme_svn_prompt_enabled no
 # ===========================
 
 set -g __fish_git_prompt_showdirtystate 'yes'
-set -g __fish_git_prompt_char_dirtystate '±'
-set -g __fish_git_prompt_char_cleanstate ''
+set -g __fish_git_prompt_char_dirtystate '●'
+set -g __fish_git_prompt_char_stagedstate '✚'
 
 function parse_git_dirty
   if [ $__fish_git_prompt_showdirtystate = "yes" ]
-    set -l submodule_syntax
-    set submodule_syntax "--ignore-submodules=dirty"
-    set untracked_syntax "--untracked-files=$fish_git_prompt_untracked_files"
-    set git_dirty (command git status --porcelain $submodule_syntax $untracked_syntax 2> /dev/null)
-    if [ -n "$git_dirty" ]
-        echo -n "$__fish_git_prompt_char_dirtystate"
-    else
-        echo -n "$__fish_git_prompt_char_cleanstate"
+    set git_staged (command git diff --cached --name-only 2> /dev/null)
+    set git_dirty (command git diff --name-only 2> /dev/null)
+    if [ -n "$git_dirty" ];
+        echo -n $__fish_git_prompt_char_dirtystate
+    end
+    if [ -n "$git_staged" ];
+        echo -n $__fish_git_prompt_char_stagedstate
     end
   end
 end
